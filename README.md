799397b80d2b2f41814fffc1fc77d0e8ce4faff7
javier <4.1.4.1.done@gmail.com>
2014-05-15 15:28:53 -0700

    update readme and check logic to just look at the queue of the potential job

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Resque::Plugins::RealSerialQueues
 
-TODO: Write a gem description
+This plugin modifies [Resque's](https://github.com/resque/resque) job reservation process to first check if there is another job 
+from the same queue as the potential job currently being processed by one of your workers and tells the worker to check again after its sleep cycle.
+
+It is primarily meant to be used to queue long running background jobs across multiple queues in conjunction with something like [resque-scheduler](https://github.com/resque/resque-scheduler)
+
+A key difference between this and [resque-lonely_job](https://github.com/wallace/resque-lonely_job) is that the queue is not touched when we check to see if we can process the job.  However, because we must check all the workers
+currently working on the system this plugin may not scale well to large numbers of workers.
 
 ## Installation
 
@@ -17,9 +23,21 @@ Or install it yourself as:
     $ gem install resque-real-serial-queues
 
 ## Usage
+  Just load up plugin somewhere in your project's initializers:
 
-This gem is under active development and may not work at all (yet)
+    require 'resque_real_serial_queues'
 
+  In rails adding
+
+    resque_real_serial_queues.rb
+
+  to your 
+
+    config/initializers
+
+  directory with the above line should do the trick.
+
+  Note that this will currently affect all queues
 ## Contributing
 
 1. Fork it ( https://github.com/[my-github-username]/resque-real-serial-queues/fork )
--- lib/resque/plugins/real_serial_queues.rb
+++ lib/resque/plugins/real_serial_queues.rb
@@ -3,43 +3,23 @@ require 'resque' unless defined?(Resque)
 require "resque/plugins/version"
 
 # Modifications to resque code go here
-module Resque # :nodoc:
-  class Job # :nodoc:
+module Resque
+  class Job
 
     # Given a queue name and a potential job (the one we are considering popping off the queue)
     # determine whether there is a worker currently working on a job with the same queue, class, and args
     # return true if we find an identical job, false if not
-    def self.same_job_in_same_queue_currently_being_run(queue, job)
+    def self.job_from_same_queue_is_currently_being_run(queue, job)
       return false unless job
 
-      job_class = job['class']
-      job_args  = job['args']
-
       Worker.working.each do |worker|
-        queue_matches = false
-        class_matches = false
-        args_match    = false
-
         processing_job = worker.job
+        next unless processing_job
 
         processing_queue = processing_job['queue']
-        processing_class = processing_job['payload']['class']
-        processing_args  = processing_job['payload']['args']
+        next unless processing_queue
 
-        queue_matches = processing_queue == queue
-        class_matches = processing_class == job_class
-        args_match    = (processing_args.nil? && job_args.nil?) ||
-                        (processing_args.length == 0 && job_args.length == 0)
-
-        unless args_match
-          processing_args.each_with_index do |arg, i|
-            next unless arg && arg[i]
-            args_match = arg == job_args[i]
-            break if args_match
-          end
-        end
-
-        return true if queue_matches && class_matches && args_match
+        return true if processing_queue == queue
       end
 
       false
@@ -49,11 +29,9 @@ module Resque # :nodoc:
     # if any jobs are available. If not, returns nil.
     # This is a resque method overridden to first check
     # if the queue item has an identical job currently being worked on
-    def self.reserve(queue) # :nodoc:
-      # Hack.  Find a way not to do this
-      return if queue == '*'
+    def self.reserve(queue)
       potential_job = Resque.peek(queue)
-      return if same_job_in_same_queue_currently_being_run(queue, potential_job)
+      return if job_from_same_queue_is_currently_being_run(queue, potential_job)
       return unless payload = Resque.pop(queue)
       new(queue, payload)
     end

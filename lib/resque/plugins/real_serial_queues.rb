54efcba4520793ef62aabe551da3c84fbb9f8ad5
javier <4.1.4.1.done@gmail.com>
2014-05-21 10:44:08 -0700

    added the ability to include a config file defining queues that you don't want to be processed serially

--- lib/resque/plugins/real_serial_queues.rb
+++ lib/resque/plugins/real_serial_queues.rb
@@ -7,9 +7,10 @@ module Resque
   class Job
 
     # Given a queue name
-    # determine whether there is a worker currently working on a job with the same queue, class, and args
-    # return true if we find an identical job, false if not
-    def self.job_from_same_queue_is_currently_being_run(queue)
+    # determine whether there is a worker currently working on a 
+    # job from the same queue
+    # return true 
+    def self.job_from_same_queue_is_currently_being_run?(queue)
       Worker.working.each do |worker|
         processing_job = worker.job
         next unless processing_job
@@ -26,9 +27,16 @@ module Resque
     # Given a string queue name, returns an instance of Resque::Job
     # if any jobs are available. If not, returns nil.
     # This is a resque method overridden to first check
-    # if the queue item has an identical job currently being worked on
-    def self.reserve(queue) 
-      return if job_from_same_queue_is_currently_being_run(queue)
+    # if there is another job from the queue being checked currently
+    # being worked on.  This check can be bypassed by setting
+    # non_serial_queues via Resque::Plugins::RealSerialQueues.config=
+    # in your Resque config
+    def self.reserve(queue)
+      non_serial_queues = Resque::Plugins::RealSerialQueues.non_serial_queues
+
+      unless non_serial_queues && non_serial_queues.include?(queue)
+        return if job_from_same_queue_is_currently_being_run?(queue)
+      end
       return unless payload = Resque.pop(queue)
       new(queue, payload)
     end
@@ -39,6 +47,37 @@ end
 module Resque
   module Plugins
     module RealSerialQueues
+      class InvalidConfigurationError < StandardError; end
+
+      class << self
+        attr_accessor :non_serial_queues
+      end
+
+      def self.config=(config_hash)
+        if config_hash
+          self.check_if_config_is_valid(config_hash)
+          self.non_serial_queues = config_hash[:non_serial_queues]
+        end
+      end
+
+      private
+
+      def self.check_if_config_is_valid(config_hash)
+        if !config_hash.is_a?(Hash)
+          fail(
+            InvalidConfigurationError,
+            'Something is wrong with your config file.  Sorry...'
+          )
+        elsif config_hash[:non_serial_queues] &&
+              !config_hash[:non_serial_queues].is_a?(Array)
+          fail(
+            InvalidConfigurationError,
+            'Please specify non-serial queue names in an array ' +
+            '(or collection if you are using YAML: http://yaml4r.' +
+            'sourceforge.net/doc/page/collections_in_yaml.htm)'
+          )
+        end
+      end
     end
   end
 end
--- test/resque_test.rb
+++ test/resque_test.rb
@@ -4,7 +4,9 @@ describe Resque::Job do
   before do
     @fake_queue = 'fake_queue'
     @worker = Resque::Worker.new(@fake_queue)
-    @job =  { # Resque checks redis for the worker's value and returns a hash decoded by MultiJSON
+    # Resque checks redis for the worker's value and 
+    # returns a hash decoded by MultiJSON
+    @job =  {
       'queue'   => @fake_queue,
       'payload' => {
         'class' => 'SomeJob',
@@ -13,40 +15,134 @@ describe Resque::Job do
     }
   end
 
-  describe '#job_from_same_queue_is_currently_being_run' do
+  describe '#job_from_same_queue_is_currently_being_run?' do
     it 'returns false if no workers' do
       Resque::Worker.expects(:working).returns([])
       Resque::Job
-        .job_from_same_queue_is_currently_being_run('some_queue').must_equal(false)
+        .job_from_same_queue_is_currently_being_run?('some_queue')
+        .must_equal(false)
     end
 
-    it 'returns true if the queue name of any processing job matches the queue name passed in' do
+    it 'returns true if the queue name of any processing ' +
+       'job matches the queue name passed in' do
+
       Resque::Worker.expects(:working).returns([@worker])
       @worker.expects(:job).returns(@job)
       Resque::Job
-        .job_from_same_queue_is_currently_being_run(@fake_queue).must_equal(true)
+        .job_from_same_queue_is_currently_being_run?(@fake_queue)
+        .must_equal(true)
     end
 
     it 'returns false if there are jobs but none from the specified queue' do
       Resque::Worker.expects(:working).returns([@worker])
       @worker.expects(:job).returns(@job)
       Resque::Job
-        .job_from_same_queue_is_currently_being_run('an_entirely_different_queue').must_equal(false)
+        .job_from_same_queue_is_currently_being_run?('a_different_queue')
+        .must_equal(false)
     end
   end
 
   describe '#reserve' do
-    it 'should return nil if a job from the requested queue is being performed' do
-      Resque::Job.expects(:job_from_same_queue_is_currently_being_run).returns(true)
-      Resque::Job.reserve('queue_name').must_equal(nil)
+    describe 'when there are no non-serial queues specified' do
+      before do
+        Resque::Plugins::RealSerialQueues.config = {}
+      end
+
+      it 'should return nil if job from requested queue is being performed' do
+        Resque::Job
+          .expects(:job_from_same_queue_is_currently_being_run?)
+          .returns(true)
+        Resque::Job.reserve('queue_name').must_equal(nil)
+      end
+
+      it 'should return at instance of Resque::Job if no ' +
+         'job from requested queue is being performed' do
+        Resque::Job
+          .expects(:job_from_same_queue_is_currently_being_run?)
+          .returns(false)
+
+        Resque.expects(:pop).returns(@job)
+
+        result = Resque::Job.reserve(@fake_queue)
+        
+        result.must_be_instance_of(Resque::Job)
+      end
     end
 
-    it 'should return at instance of Resque::Job if no job from requested queue is being performed' do
-      Resque::Job.expects(:job_from_same_queue_is_currently_being_run).returns(false)
-      Resque.expects(:pop).returns(@job)
-      result = Resque::Job.reserve(@fake_queue)
+    describe 'when there are non-serial queues specified' do
+      before do
+        @serial_queue = 'so_serial'
+        @non_serial   = 'not_so_serial'
+        Resque::Plugins::RealSerialQueues.config = {
+          non_serial_queues: [@non_serial]
+        }
+
+        Resque.stubs(:pop).returns(@job)
+      end
       
-      result.must_be_instance_of(Resque::Job)
+      describe 'and there is a job from the same queue being performed' do
+        before do
+          Resque::Job.stubs(:job_from_same_queue_is_currently_being_run?)
+            .returns(true)
+        end
+
+        it 'should return an Resque::Job instance if in the non-serial queue' do
+          result = Resque::Job.reserve(@non_serial)
+          result.must_be_instance_of(Resque::Job)
+        end
+
+        it 'should return nil if not in the non-serial queue' do
+          result = Resque::Job.reserve(@serial_queue)
+          result.must_be_nil
+        end
+      end
+
+      describe 'and there is no job from the same queue being performed' do
+        before do
+          Resque::Job.stubs(:job_from_same_queue_is_currently_being_run?)
+            .returns(false)
+        end
+
+        it 'should return a Resque::Job instance if in non-serial queue' do
+          result = Resque::Job.reserve(@non_serial)
+          result.must_be_instance_of(Resque::Job)
+        end
+
+        it 'should return a Resque::Job instance if in serial queue' do
+          result = Resque::Job.reserve(@serial_queue)
+          result.must_be_instance_of(Resque::Job)
+        end
+      end
+    end
+  end
+end
+
+describe Resque::Plugins::RealSerialQueues do
+  describe '#config=' do
+    it 'should take in the config hash in a way that is accessible' do
+      Resque::Plugins::RealSerialQueues.config = {
+        non_serial_queues:[:camera, :bear]
+      }
+
+      Resque::Plugins::RealSerialQueues
+        .non_serial_queues
+        .must_equal([:camera, :bear])
+    end
+
+    it 'should throw an error if the config is not a Hash' do
+      -> {
+        Resque::Plugins::RealSerialQueues.config = 'good morning bear'
+      }.must_raise(
+        Resque::Plugins::RealSerialQueues::InvalidConfigurationError
+      )
+    end
+
+    it 'should throw an error if non serial queues not specified in an array' do
+      -> { 
+        Resque::Plugins::RealSerialQueues.config = {
+          non_serial_queues: 'best_queue, better_queue'
+        } 
+      }.must_raise(Resque::Plugins::RealSerialQueues::InvalidConfigurationError)
     end
   end
 end
